05b2a772c30fa09b7f9bd8aba64a60c93288d471
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-11 20:21:28 -0500

    img added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import memories from './images/memories.png';
 
 const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar position='static' color='inherit'>
                 <Typography variant='h2' align='center'>Memories</Typography>
-                {/* <img src={memories} alt='memories' height="60" /> */}
+                <img src={memories} alt='memories' height="60" />
             </AppBar>
         </Container>
     );

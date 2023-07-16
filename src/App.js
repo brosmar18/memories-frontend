09da54cffab6a063d4561656916125bfd1370470
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-16 12:07:40 -0500

    redux hook created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Posts from './Components/Posts/Posts';
 import NavBar from './Components/NavBar/NavBar';
+import {useDispatch} from 'react-redux';
 
 
 const App = () => {
+    const dispatch = useDispatch();
+    
     return (
         <div className='page'>
             <NavBar />

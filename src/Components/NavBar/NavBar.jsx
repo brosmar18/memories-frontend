552174d3bb208d50e6cac93f8eaac210a7a315c5
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-16 10:43:49 -0500

    NavBar added

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 import memories from '../../images/memories.png';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className='bg-body-tertiary'>
+    <Navbar bg='dark' data-bs-theme='dark' expand="lg" className='bg-body-tertiary'>
         <Container>
             <Navbar.Brand href='#'>
                 <img className='nav__img' src={memories} alt='memories' />
             </Navbar.Brand>
-            <Navbar.Toggle id='basic-navbar-nav'>
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link href='#'>Home</Nav.Link>
-                        <Nav.Link href='#'>Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar.Toggle>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='me-auto'>
+                    <Nav.Link href='#'>Home</Nav.Link>
+                    <Nav.Link href='#'>Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Container>
     </Navbar>
   )

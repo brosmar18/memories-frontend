f212bc176a65430bc3b21e3d442fd1161f9eaf56
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-16 11:03:59 -0500

    redux installed and setup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import {applyMiddleware, compose, legacy_createStore} from 'redux';
+import {legacy_createStore as createStore} from 'redux';
 import thunk from 'redux-thunk';
 
+import reducers from './Reducers';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);

f69d2ae4c046cbe2a96b3cd629ee594c6a32d95b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-16 12:06:05 -0500

    imports updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux';
-import {applyMiddleware, compose, legacy_createStore} from 'redux';
+import {applyMiddleware, compose} from 'redux';
 import {legacy_createStore as createStore} from 'redux';
 import thunk from 'redux-thunk';
 

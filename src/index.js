52a09384590d7cf70748d98d39f92e74a1160a17
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-16 10:53:55 -0500

    redux imported

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);

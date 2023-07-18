3fe09bd0cb7e18a9d140c5727397ed3f338cdfd4
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-17 22:03:36 -0500

    Card body and button added

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -13,6 +13,13 @@ const Posts = () => {
         </Spinner> : (
             <Card style={{width: '18rem'}}>
                 <Card.Img variant="top" src='https://placekitten.com/640/360' />
+                <Card.Body>
+                    <Card.Title>Card Titler</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of the card's content
+                    </Card.Text>
+                    <Button variant='primary'>Go Somewhere</Button>
+                </Card.Body>
             </Card>
         )
     );
